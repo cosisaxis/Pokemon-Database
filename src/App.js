d17119af8d8cd978818f8e1686164a21f3d9863a
cosisaxis <kishanelewis2@gmail.com>
2022-03-24 16:29:24 -0500

    style-change

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
          name: "",
          species: "",
          img: "",
-         hp: "",
+         
          attack: "",
          defense: "",
          type: "",
@@ -22,7 +22,6 @@ function App() {
       (response) => {
          setPokemon({name:pokemonName, species: response.data.species.name, 
           img: response.data.sprites.front_default, 
-          hp: response.data.stats[0].base_stat, 
           attack: response.data.stats[1].base_stat,
           defense: response.data.stats[0].base_stat,
           type: response.data.types[0].type.name,
@@ -35,27 +34,35 @@ function App() {
   return (
     <div className= "App">
      <div className="TitleSection">
-       <h1>Pokemon Database</h1>
-       <input type = "text" placeholder = "enter pokemon" onChange ={(event) => {
+       <div className='title-container'>
+       <h1>Pokedex</h1>
+       <input className='input-edit' type = "text" placeholder = "enter pokemon" onChange ={(event) => {
          setPOkemonName(event.target.value);
          }} />
-       <button onClick={searchPokemonName}>Search Pokemon</button>
+       <button onClick={searchPokemonName}>Search</button>
+       </div>
      </div>
+     
      <div className="DisplaySection">  
+      <div className='display-results'>
         {!pokemonChosen ? (
-        <h1>Pick a pokemon</h1>
+        <h1></h1>
         ) : (
         <>
-         <h1>{pokemon.name}</h1>
-         <img src={pokemon.img}/>
-         <h3>Species: {pokemon.species}</h3>
-         <h3>Type: {pokemon.type}</h3>
-         <h3>Hp: {pokemon.type}</h3>
-         <h3>Atttack: {pokemon.attack}</h3>
-         <h3>Defense: {pokemon.defense}</h3>
+         
+         <img src={pokemon.img} />
+         <p>Name: {pokemon.name}</p>
+         <p>Type: {pokemon.type}</p>
+         <p>Atttack: {pokemon.attack}</p>
+         <p>Defense: {pokemon.defense}</p>
         </>
         )}
-     </div>
+        </div>
+        <div className='footer'>
+          <p>made by <a className='name-link' href='https://twitter.com/cartieraxis'>Axis</a></p>
+
+        </div>
+      </div>
     </div>
   );
 }
